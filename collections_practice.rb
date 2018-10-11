7c8b5d581f9568cc07ecba303ae3b2d37359c0f4
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 23:01:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
 # your code goes here
+def begins_with_r(arr)
+  arr.all? {|word| word.downcase.chr == "r"}
+end
+
+def contain_a(arr)
+  arr.find_all {|x| x.include? "a"}
+end
+
+def first_wa(arr)
+  arr.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(arr)
+  arr.reject {|word| word.class != String}
+end
+
+def count_elements(arr)
+  arr.group_by(&:itself).map{|k, v| k.merge(count: v.length)}
+end 
+
+def merge_data
+  
+end 
+
+def find_cool
+  
+end
+
+def organize_schools
+  
+end
